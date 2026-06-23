51afa0bb1e92a1904a0af24246994e6189db47e4
JiHyoung You <fbwlgud51@gmail.com>
2026-06-23 22:48:06 +0900

    wip: 무한 스크롤 구현 중

--- src/api/spot/spotApi.ts
+++ src/api/spot/spotApi.ts
@@ -1,18 +1,39 @@
 import axios from "axios";
-import type { ISpotRequest, ISpotResponse } from "../../types/spot";
+import type {
+  ISearchByKeywordRequest,
+  ISearchByLocationRequest,
+  ISearchSpotResponse,
+} from "../../types/spot";
 
 export const getSpotsByKeyword = async ({
   pageNo,
   keyword,
-}: ISpotRequest): Promise<ISpotResponse[]> => {
+}: ISearchByKeywordRequest): Promise<ISearchSpotResponse[]> => {
   try {
-    const response = await axios.get(
-      "https://port-0-jeju-trip-mq3obdfx86be820f.sel3.cloudtype.app/search/keyword",
-      { params: { pageNo, keyword } },
-    );
+    const response = await axios.get("/api/search/keyword", {
+      params: { pageNo, keyword },
+    });
     return response.data.data;
   } catch (error) {
     console.error("Fetch Spot by Keyword Error: ", error);
     throw new Error("Fail to fetch Spot by Keyword.", { cause: error });
+  }
+};
+
+export const getSpotsByLocation = async ({
+  pageNo,
+  mapX,
+  mapY,
+  radius,
+}: ISearchByLocationRequest): Promise<ISearchSpotResponse[]> => {
+  try {
+    const response = await axios.get("/api/search/location", {
+      params: { pageNo, mapX, mapY, radius },
+    });
+    console.log("pageNo:", pageNo, "data:", response.data.data);
+    return response.data.data;
+  } catch (error) {
+    console.error("Fetch Spot by Location Error: ", error);
+    throw new Error("Fail to fetch Spot by Location.", { cause: error });
   }
 };
--- src/hooks/spot/useGetSpotsByKeyword.ts
+++ src/hooks/spot/useGetSpotsByKeyword.ts
@@ -1,11 +1,19 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { getSpotsByKeyword } from "../../api/spot/spotApi";
-import type { ISpotRequest } from "../../types/spot";
+import type { ISearchByKeywordRequest } from "../../types/spot";
 
-export const useGetSpotsByKeyword = ({ keyword, pageNo }: ISpotRequest) => {
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ["spot-keyword", keyword, pageNo],
-    queryFn: () => getSpotsByKeyword({ pageNo, keyword }),
+export const useGetSpotsByKeyword = ({
+  keyword,
+}: Omit<ISearchByKeywordRequest, "pageNo">) => {
+  return useInfiniteQuery({
+    queryKey: ["spot-keyword", keyword],
+    queryFn: ({ pageParam }) =>
+      getSpotsByKeyword({ pageNo: pageParam, keyword }),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages) => {
+      if (lastPage.length === 0) return undefined;
+      return allPages.length + 1;
+    },
+    staleTime: 1000 * 60 * 60 * 3, // 3시간
   });
-  return { data, isLoading, isError };
 };
