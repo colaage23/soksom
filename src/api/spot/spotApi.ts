1483713d641804b58d6ccbcc57271a4d45ec971b
JiHyoung You <fbwlgud51@gmail.com>
2026-06-28 03:27:26 +0900

    fix: axios 제네릭 타입 추가하여 타입 추론 오류 해결

--- src/api/spot/spotApi.ts
+++ src/api/spot/spotApi.ts
@@ -10,9 +10,12 @@ export const getSpotsByKeyword = async ({
   keyword,
 }: ISearchByKeywordRequest): Promise<ISearchSpotResponse[]> => {
   try {
-    const response = await axios.get("/api/search/keyword", {
-      params: { pageNo, keyword },
-    });
+    const response = await axios.get<{ data: ISearchSpotResponse[] }>(
+      "/api/search/keyword",
+      {
+        params: { pageNo, keyword },
+      },
+    );
     return response.data.data;
   } catch (error) {
     console.error("Fetch Spot by Keyword Error: ", error);
@@ -27,9 +30,12 @@ export const getSpotsByLocation = async ({
   radius,
 }: ISearchByLocationRequest): Promise<ISearchSpotResponse[]> => {
   try {
-    const response = await axios.get("/api/search/location", {
-      params: { pageNo, mapX, mapY, radius },
-    });
+    const response = await axios.get<{ data: ISearchSpotResponse[] }>(
+      "/api/search/location",
+      {
+        params: { pageNo, mapX, mapY, radius },
+      },
+    );
     console.log("pageNo:", pageNo, "data:", response.data.data);
     return response.data.data;
   } catch (error) {
